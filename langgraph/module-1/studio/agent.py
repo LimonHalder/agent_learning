ebf891dd3a1e2daf3243cad504d08f1e95c59ee5
Shaon2221 <18596945+Shaon2221@users.noreply.github.com>
2024-11-19 14:26:01 +0600

    Refactor langgraph configuration and update agent model initialization

--- langgraph/module-1/studio/agent.py
+++ langgraph/module-1/studio/agent.py
@@ -1,5 +1,6 @@
 from langchain_core.messages import SystemMessage
 from langchain_openai import ChatOpenAI
+import os
 
 from langgraph.graph import START, StateGraph, MessagesState
 from langgraph.prebuilt import tools_condition, ToolNode
@@ -34,7 +35,10 @@ def divide(a: int, b: int) -> float:
 tools = [add, multiply, divide]
 
 # Define LLM with bound tools
-llm = ChatOpenAI(model="gpt-4o")
+llm = ChatOpenAI(model="gpt-4o",
+                 base_url=os.getenv("BASE_URL"),
+                 api_key=os.getenv("OPENAI_API_KEY")
+                 )
 llm_with_tools = llm.bind_tools(tools)
 
 # System message
